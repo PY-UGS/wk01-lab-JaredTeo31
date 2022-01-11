881e670cb758fa6274b31dc234ca4e28010b3a01
JaredTeo31 <97490330+JaredTeo31@users.noreply.github.com>
2022-01-11 14:13:27 +0800

    Delete Question2A.java

--- Question2A.java
+++
@@ -1,9 +0,0 @@
-class Question2A 
-{
-    public static void main(String[] args) 
-    {
-      System.out.println("Question 2a: ");
-      System.out.println("Hello, I am Jared");
-    }
-      
-}
