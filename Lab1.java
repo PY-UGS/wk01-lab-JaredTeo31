5ffe2f519899d1514b46530a870a2e48f3d6b208
JaredTeo31 <97490330+JaredTeo31@users.noreply.github.com>
2022-01-11 14:16:59 +0800

    Add files via upload

---
+++ Lab1.java
@@ -0,0 +1,37 @@
+class Question2 
+{
+    public static void main(String[] args) 
+    {
+      System.out.println("Question 2a: ");
+      System.out.println("Hello, I am Jared");
+      System.out.println("\nQuestion 2b: ");
+      String module = "CSC1009";
+      switch(module)
+      {
+        case "CSC1009":
+        System.out.println("Object-Oriented Programming");
+        break;
+        case "CSC1008":
+        System.out.println("Data structures and algorithm");
+        break;
+        case "CSC1007":
+        System.out.println("Operating systems");
+        break;
+        case "CSC1006":
+        System.out.println("Mathematics 2");
+        break;
+        default:
+        System.out.println("Unknown Module");
+        break;
+      }
+
+      System.out.println("\nQuestion 2c: ");
+      for (int i = 102; i >= 66; i--) 
+      {
+        if (i % 2 != 0) {
+          int odd = i;
+          System.out.println("Value of i=" + odd);
+      }
+    }
+  }
+}
