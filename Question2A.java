120fa5148f26a01bb2d5b818d7c8e52f824bb45a
JaredTeo31 <97490330+JaredTeo31@users.noreply.github.com>
2022-01-11 14:12:24 +0800

    Add files via upload

---
+++ Question2A.java
@@ -0,0 +1,9 @@
+class Question2A 
+{
+    public static void main(String[] args) 
+    {
+      System.out.println("Question 2a: ");
+      System.out.println("Hello, I am Jared");
+    }
+      
+}
---
+++ Question2C.java
@@ -0,0 +1,11 @@
+class Question2C {
+    public static void main(String[] args) {
+      System.out.println("Question 2c: ");
+      for (int i = 102; i >= 66; i--) {
+        if (i % 2 != 0) {
+          int odd = i;
+          System.out.println("Value of i=" + odd);
+      }
+      }
+    }
+  }
